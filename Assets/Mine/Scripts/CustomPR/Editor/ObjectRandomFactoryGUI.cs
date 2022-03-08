4ededa36060bf0eb9143fc249972bd75dcd90e08
hanjun <274203544@qq.com>
2022-03-08 15:39:33 +0800

    准备场景。增加工具类 用于生成物体

---
+++ Assets/Mine/Scripts/CustomPR/Editor/ObjectRandomFactoryGUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ObjectRandomFactory))]
+public class ObjectRandomFactoryGUI : Editor
+{
+    ObjectRandomFactory targetScript;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        this.targetScript = target as ObjectRandomFactory;
+
+        if (this.targetScript == null)
+        {
+            return;
+        }
+
+        if (GUILayout.Button("创建对象"))
+        {
+            this.targetScript.CreateOne();
+        }
+    }
+}
+
---
+++ Assets/Mine/Scripts/CustomPR/Runtime/Tools/ObjectRandomFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRandomFactory : MonoBehaviour
+{
+    [Tooltip("随机物体")]
+    public GameObject item;
+    public Collider area;
+
+
+    public void CreateOne()
+    {
+        if (area != null && item != null)
+        {
+            var randomPoint = Random.insideUnitSphere * this.area.bounds.extents.sqrMagnitude;
+            var closedPoint = this.area.bounds.ClosestPoint(randomPoint);
+
+            var dir = closedPoint - this.area.bounds.center;
+            var endPos = this.area.bounds.center + Random.Range(0f, 1f) * dir;
+
+            Instantiate(this.item, endPos, Quaternion.identity, this.transform);
+        }
+    }
+}
